032642eea00511794ce1607ef2b3e1e8d273e852
Patrick Ngo <patrick.ngo@hotmail.com>
2016-05-27 22:00:21 +0800

    added POST requests to BlogPostAPI (@Body and @Field)

--- app/src/main/java/ngo/patrick/netccentricassignment/http/BlogPostAPI.java
+++ app/src/main/java/ngo/patrick/netccentricassignment/http/BlogPostAPI.java
@@ -2,10 +2,15 @@ package ngo.patrick.netccentricassignment.http;
 
 import ngo.patrick.netccentricassignment.model.BlogPost;
 import ngo.patrick.netccentricassignment.model.BlogPostList;
+import ngo.patrick.netccentricassignment.model.Result;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -18,8 +23,16 @@ public interface BlogPostAPI
 
     @GET("posts/{id}/")
     Call<BlogPost> getSingleBlogPost(
-        @Path("id") String id);
+            @Path("id") String id);
 
+    @POST("posts/")
+    Call<Result> createBlogPost(
+            @Body Result result);
+
+    @FormUrlEncoded
+    @POST("posts/")
+    Call<Result> updateBlogPost(
+            @Field("caption") String caption);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://demo6401519.mockable.io/")
